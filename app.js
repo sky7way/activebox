31f76453724c03d8ba53067d35ac7a183a3a708e
Aleksandr <sky7way@yandex.ru>
2024-02-08 21:44:24 +0300

    slider reviews

--- app.js
+++ app.js
@@ -49,4 +49,18 @@ $(function() {
     });
 
 
+    // Reviews  https://kenwheeler.github.io/slick/
+
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+      });
+
+
 });
