ccbd0d8b71c66379f65290724317114c1a2bbbb8
Aleksandr <sky7way@yandex.ru>
2024-02-08 19:52:58 +0300

    fix header & scroll

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 $(function() {
+
+    // Fixed header
     let header = $("#header");
     let intro = $("#intro"); 
-    let introH; 
+    let introH = intro.outerHeight(); 
     let scrollPos = $(window).scrollTop(); 
 
+    checkScroll(scrollPos, introH);
+
     $(window).on("scroll load resize", function () {
-        intro.outerHeight();
+        introH = intro.outerHeight();
         scrollPos = $(this).scrollTop();
 
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
         if(introH < scrollPos) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
+    }
+
+    // Smooth scroll
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementId = $(this).data("scroll");
+        let elementOffset = $(elementId).offset().top;
+        console.log(elementOffset);
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 60
+        }, 800)
     });
+
+
 });
